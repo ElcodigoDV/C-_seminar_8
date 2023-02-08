d19b9b261c5a9527f91323e83fad07e80bc8587c
DmitriyK <d_kurban@yahoo.com>
2023-02-09 01:17:57 +1000

    ex_54, 56 is done

---
+++ ex_54/Program.cs
@@ -0,0 +1,79 @@
+﻿/* Урок 8. Двумерные массивы. Продолжение
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+ */
+
+int[,] RowSort(int[,] array)
+{
+    for (int i=0; i < array.GetLength(0); i++)
+    {
+        int tmp;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j+1; k < array.GetLength(1); k++)
+            {
+                if (array[i,j] < array[i, k])
+                {
+                    tmp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = tmp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+void ShowResult (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+    Console.WriteLine();
+    }
+}
+
+int[,] NewMatrix(int row, int col)
+{
+    int[,] newMatrix = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            newMatrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return newMatrix;
+}
+
+int Num(string text)
+{
+    Console.WriteLine(text);
+    int num = Int32.Parse(Console.ReadLine());
+    return num;
+}
+
+//задаем матрицу
+int n = Num("введите кол-во строк");
+int m = Num("введите кол-во столбцов");
+Console.WriteLine("----------------");
+int[,] matrix = NewMatrix(n, m);
+
+//выводим м-цу
+ShowResult(matrix);
+
+//сортируем м-цу
+RowSort(matrix);
+Console.WriteLine("----------------");
+// выводим рез-щую м-цу
+ShowResult(matrix);
---
+++ ex_56/Program.cs
@@ -0,0 +1,97 @@
+﻿/* Урок 8. Двумерные массивы. Продолжение
+
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу,
+которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки
+с наименьшей суммой элементов: 1 строка
+ */
+int NumRow (int[] array)
+{
+    int min = array[0];
+    int index = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+            index = i;
+        }
+    }
+    int row = index +1;
+    return row;
+}
+
+int[] ArraySum(int[,] array)
+{
+    int[] arraySum = new int[array.GetLength(0)];
+    // for (int k = 0; k < arraySum.Length; k++)
+    // {
+        int k = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            int sum = 0;
+            for (int j = 0; j< array.GetLength(1); j++)
+            {
+                sum = sum + array[i, j];
+            }
+            arraySum[k] = sum;
+            k++;
+        }
+    // }
+    Console.WriteLine("---------------"); //разделитель
+    Console.WriteLine($"{string.Join(", ", arraySum)} суммы по строкам");
+    return arraySum;
+}
+
+void ShowResult(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] NewMatrix(int row, int col)
+{
+    int[,] newMatrix = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            newMatrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return newMatrix;
+}
+
+int Num(string text)
+{
+    Console.WriteLine(text);
+    int num = Int32.Parse(Console.ReadLine());
+    return num;
+}
+
+//задаем массив
+int n = Num("введите кол-во строк");
+int m = Num("введите кол-во столбцов");
+int[,] matrix = NewMatrix(n, m);
+//показываем массив
+ShowResult(matrix);
+//считаем минимум по строке и возвращаем номер строки
+int[] arraySum = ArraySum(matrix);
+int numRow = NumRow (arraySum);
+//показываем номер строки
+Console.WriteLine();
+Console.WriteLine($"{numRow} строка");
